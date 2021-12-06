fb94541f19915278ca74d86d825dd45c8f69d988
unknown <archerzou>
2021-12-07 01:10:45 +0800

    create the structure of blog

---
+++ components/index.jsx
@@ -0,0 +1,6 @@
+export { default as PostCard } from './PostCard';
+
+export { default as Layout } from './Layout';
+export { default as Categories } from './Categories';
+
+export { default as PostWidget } from './PostWidget';
